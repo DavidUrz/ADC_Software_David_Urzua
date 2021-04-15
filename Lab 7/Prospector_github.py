e32431c510d636409a39fe1641f2f8e041e67f00
DavidUrz <49179418+DavidUrz@users.noreply.github.com>
2021-04-14 22:46:19 -0500

    Update Prospector_github.py

--- Lab 7/Prospector_github.py
+++ Lab 7/Prospector_github.py
@@ -4,5 +4,5 @@ clone = "git clone https://github.com/DavidUrz/ADC_Software_David_Urzua.git"
 path = os.getcwd()
 
 os.system("cd " + path)
-# os.system(clone)
+os.system(clone)
 os.system("prospector --o text:report_prospector_fixed.txt")
